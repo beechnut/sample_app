9d63f5f98cdffbcfc3ba72548cae3334b3c0fd72
beechnut <cloydster@gmail.com>
2013-01-27 16:44:24 -0500

    Add relationships without validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,7 +26,11 @@ describe User do
 
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+  
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+
 
   it { should_not respond_to(:admin!) }
 
