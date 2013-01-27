fe40057ae531d0910d3e4c242ea70140cb5216f2
beechnut <cloydster@gmail.com>
2013-01-27 18:53:19 -0500

    Add non-functional follow button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 SampleApp::Application.routes.draw do
-  resources :users # all REST actions for users now available
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users do # all REST actions for users now available
+    member do
+      get :following, :followers # allows users/following, users/followers
+    end
+  end
+
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
   root to: 'static_pages#home'
 
   match '/signup',    to: 'users#new'
